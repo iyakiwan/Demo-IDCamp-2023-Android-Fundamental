a4ed34cb859e8c43c611c768db6135be0f177ea8
Mufti Alie Satriawan <iyakiwan19@gmail.com>
2024-02-10 00:36:02 +0700

    fix: Add Logging

--- app/src/main/java/com/mufti/bangkit/learn/ilt3/example/ui/main/MainActivity.kt
+++ app/src/main/java/com/mufti/bangkit/learn/ilt3/example/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mufti.bangkit.learn.ilt3.example.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.mufti.bangkit.learn.ilt3.example.data.Result
@@ -19,6 +20,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var adapter: UserAdapter
 
+    companion object {
+        const val TAG = "MainActivity Log"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,6 +40,16 @@ class MainActivity : AppCompatActivity() {
 
         setupRecyclerView()
         setupView()
+
+        showLog()
+    }
+
+    private fun showLog() {
+        Log.e(TAG, "Log Error")
+        Log.w(TAG, "Log Warning")
+        Log.i(TAG, "Log Info")
+        Log.d(TAG, "Log Debug")
+        Log.v(TAG, "Log Verbose")
     }
 
     private fun setupView() {
